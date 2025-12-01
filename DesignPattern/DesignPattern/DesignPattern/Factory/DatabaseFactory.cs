0ececb369b73aef672b8dbf3820a35124b895100
SMurat2141 <serkan22095377@gmail.com>
2025-12-01 17:48:22 +0100

    Add files via upload

---
+++ DesignPattern/DesignPattern/DesignPattern/Factory/DatabaseFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPattern.Factory
+{
+    public static class DatabaseFactory
+    {
+        public static IDatabase CreateDatabase(DatabaseType type) =>
+            type switch
+            {
+                DatabaseType.SqlServer => new SqlServerDatabase(),
+                DatabaseType.Oracle   => new OracleDatabase(),
+                _ => throw new ArgumentOutOfRangeException(nameof(type), type, "Unsupported database type")
+            };
+    }
+}
---
+++ DesignPattern/DesignPattern/DesignPattern/Observer/Observer.cs
@@ -0,0 +1,31 @@
+﻿// In case you need some guidance: https://refactoring.guru/design-patterns/observer
+
+namespace DesignPattern.Observer
+{
+    public class ConcreteObserver(string userName) : IObserver
+    {
+        public string UserName { get; } = userName;
+        private ISubject? _subject;
+
+        // 3. Registering the Observer with the Subject
+        public void AddSubscriber(ISubject subject)
+        {
+            _subject = subject;
+            subject.RegisterObserver(this);
+        }
+
+        // 4. Removing the Observer from the Subject
+        public void RemoveSubscriber(ISubject subject)
+        {
+            subject.RemoveObserver(this);
+            if (ReferenceEquals(_subject, subject))
+            {
+                _subject = null;
+            }
+        }
+
+        // 5. Observer will get a notification from the Subject using this Method
+        public void Update(string availability) =>
+            Console.WriteLine($"Hello {UserName}, product availability changed to: {availability}");
+    }
+}
